c10b4213a82c2bbb5091e0e8a6685084d73562f1
Budiarno <budiarno@pedode.com>
2019-01-03 11:21:33 -0500

    Deleted files/folders due to wrong commit.

--- dino-linux-4.4.35/arch/arm/mach-hi3798cv2x/include/mach/timex.h
+++
@@ -1,6 +0,0 @@
-#ifndef __HISI_TIMEX__
-#define __HISI_TIMEX__
-
-#define CLOCK_TICK_RATE	25000000
-
-#endif
--- dino-linux-4.4.35/include/linux/hisilicon/platform.h
+++
@@ -1,4 +0,0 @@
-
-#ifdef CONFIG_ARCH_HI3796MV2X
-#include "drivers/hisilicon/soc/hi3796mv200/platform.h"
-#endif
